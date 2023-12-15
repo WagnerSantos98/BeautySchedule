7333d02082fb9d96683fedd8957bfbfa99825694
WagnerSantos98 <wagner.jesus98@outlook.com>
2023-12-15 09:50:45 -0300

    feat: relacionamento salao/colaborador

--- ws/src/models/relationship/salaoColaborador.js
+++ ws/src/models/relationship/salaoColaborador.js
@@ -1,35 +1,23 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-//Criação da tabela Salao
-const salao = new Schema({
-    nome: {
-        //Requisição de inserção de dados
+//Relacionamento Salão/Colaborador
+const salaoColaborador = new Schema({
+    salaoId:{
+        type: mongoose.Types.ObjectId,
+        ref: 'Salao',
+        required: true
+    },
+    colaboradorId:[{
+        type: mongoose.Types.ObjectId,
+        ref: 'Salao',
+        required: true
+    }],
+    status: {
         type: String,
-        required: [true, 'Nome é obrigatório.']
-    },
-    foto: String,
-    capa: String,
-    email: {
-        //Requisição de inserção de dados
-        type: String,
-        required: [true, 'E-mail é obrigatório.']
-    },
-    senha: {
-        type: String,
-        default: null
-    },
-    telefone: String,
-    endereco: {
-        cidade: String,
-        uf: String,
-        cep: String,
-        numero: String,
-        pais: String,
-    },
-    geo:{
-        tipo: String,
-        coordinates: Array,
+        required: true,
+        enum: ['A', 'I'],
+        default: 'A'
     },
     dataCadastro: {
         type: Date,
@@ -37,6 +25,5 @@ const salao = new Schema({
     },
 });
 
-salao.index({geo: '2dsphere'});
 
-module.exports = mongoose.model('Salao', salao);
+module.exports = mongoose.model('SalaoColaborador', salaoColaborador);
