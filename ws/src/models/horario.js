66258931b84b98cde7625317f57e566a6fb4f5b6
WagnerSantos98 <wagner.jesus98@outlook.com>
2023-12-18 12:05:54 -0300

    feat: router horário

--- ws/src/models/horario.js
+++ ws/src/models/horario.js
@@ -8,25 +8,11 @@ const horario = new Schema({
         ref: 'Salao',
         required: true
     },
-    especialidades:[{
-        type: mongoose.Types.ObjectId,
-        ref: 'Servico',
-        required: true
-    }],
-    colaboradores:[{
-        type: mongoose.Types.ObjectId,
-        ref: 'Colaborador',
-        required: true
-    }],
-    dias:{
-        type: [Number]
-    },
-    inicio:{
-        type: Date
-    },
-    fim:{
-        type: Date
-    },
+    especialiodades: String,
+    colaboradores: String,
+    dias: Number,
+    inicio: Date,
+    fim: Date,
     dataCadastro: {
         type: Date,
         default: Date.now(),
--- ws/src/routes/.js
+++
@@ -1,9 +0,0 @@
-{
-    "salaoId": "657dd607a0bb44115af4e8ca",
-    "titulo": "Serviço Teste",
-    "preco": 20,
-    "comissao": 10,
-    "duracao": 30,
-    "recorrencia": 30,
-    "descricao": "Descrição Teste"
-}
