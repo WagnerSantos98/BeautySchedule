5f1efb7d13c349e1f572dbac44bfc957df206405
WagnerSantos98 <wagner.jesus98@outlook.com>
2023-12-19 11:13:35 -0300

    feat: implementação de update e delete file

--- ws/src/routes/servico.routes.js
+++ ws/src/routes/servico.routes.js
@@ -7,6 +7,7 @@ const busboyBodyParser = require('busboy-body-parser');
 
 router.use(busboyBodyParser());
 
+//Rota de inserção - INSERT (POST)
 router.post('/', async (req, res) => {
     
         try {
@@ -56,10 +57,76 @@ router.post('/', async (req, res) => {
         }catch (err) {
             res.json({error: true, message: err.message});
         }
-    });
+});
+
+//Rota de atualização - UPDATE (PUT)
+router.put('/:id', async (req, res) => {
     
+    try {
+        const { salaoId, servico } = req.body;
+        let errors = [];
+        let arquivos = [];
 
+        if(req.files && Object.keys(req.files).length > 0){
+            for(let key of Object.keys(req.files)){
+                const file = req.files[key];
 
+                const nameParts = file.name.split('.'); // [123123123, jpg]
+                const fileName = `${new Date().getTime()}.${
+                    nameParts[nameParts.length - 1]}`;
+
+                const path = `servicos/${salaoId}/${fileName}`;
+
+                const response = await aws.uploadToS3(file, path);
+
+                if(response.error){
+                    errors.push({ error: true, message: response.message})
+                }else{
+                    arquivos.push(path);
+                }
+            }
+        }
+
+        if(errors.length > 0){
+            res.json(errors[0]);
+            return false;
+        }
+
+        //Criar Serviço
+        const jsonServico = JSON.parse(servico);
+        await Servico.findByIdAndUpdate(req.params.id, jsonServico);
+
+        //Criar Arquivo
+        arquivos = arquivos.map((arquivo) => ({
+            referenciaId: req.params.id,
+            model: 'Servico',
+            caminho: arquivo,
+        }));
+
+        await Arquivo.insertMany(arquivos);
+
+        res.json({ error: false });
+    }catch (err) {
+        res.json({error: true, message: err.message});
+    }
+});
+
+//Rota de exclusão - DELETE (DELETE)
+router.post('/delete-arquivo', async (req, res) => {
+    try{
+        const { id } = req.body;
+
+        //Excluir AWS
+        await aws.deleteFileS3(id);
+
+        await Arquivo.findOneAndDelete({
+            caminho: id,
+        });
+        res.json({error: false});
+    }catch (err) {
+        res.json({error: true, message: err.message});
+    }
+});
 
 
 module.exports = router;
