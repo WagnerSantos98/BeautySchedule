fb3124be5c9c4cb26bd10c408d0c987cdf18c9df
WagnerSantos98 <wagner.jesus98@outlook.com>
2023-12-19 17:38:26 -0300

    refactor: finalização de routes horarios, com exclusão

--- ws/src/routes/horario.routes.js
+++ ws/src/routes/horario.routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Horario = require('../models/horario');
 
+//Rota de inserção - INSERT (POST)
 router.post('/', async(req, res) => {
     try{
         const horario = await new Horario(req.body).save();
@@ -11,12 +12,38 @@ router.post('/', async(req, res) => {
     }
 });
 
+//Rota de retorno de informmação - GET
+router.get('/salao/:salaoId', async(req, res) => {
+    try{
+        const {salaoId} = req.params;
+        const horarios = await  Horario.find({salaoId});
+
+        res.json({ horarios });
+    }catch(err){
+        res.json({error: true, message: err.message});
+    }
+})
+
+//Rota de atualização - UPDATE (PUT)
 router.put('/:horarioId', async (req, res) => {
     try{
         const { horarioId } = req.params;
         const horario = req.body;
 
         await Horario.findByIdAndUpdate(horarioId, horario);
+
+        res.json({error: false });
+    }catch(err) {
+        res.json({error: true, message: err.message});
+    }
+});
+
+//Rota de EXCLUSÃO - DELETE
+router.delete('/:horarioId', async (req, res) => {
+    try{
+        const { horarioId } = req.params;
+
+        await Horario.findByIdAndDelete(horarioId);
 
         res.json({error: false });
     }catch(err) {
