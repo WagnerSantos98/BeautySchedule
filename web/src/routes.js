590340fc1effbb26b41f85a638fb50d923b999b8
WagnerSantos98 <wagner.jesus98@outlook.com>
2023-12-20 19:56:04 -0300

    feat: implementação de rotas painel web

--- web/src/routes.js
+++ web/src/routes.js
@@ -1,5 +1,30 @@
-const Routes = () => {
-    return <h1>Teste</h1>
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import './styles.css';
+
+import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
+
+import Agendamentos from "./pages/Agendamentos";
+import Clientes from "./pages/Clientes";
+
+const App = () => {
+    return (
+        <>
+            <Header/>
+            <div className="container-fluid h-100">
+                <div className="row h-100">
+                    <Router>
+                        <Sidebar/>
+                        <Routes>
+                            <Route path="/" excat element={<Agendamentos />}/>
+                            <Route path="/clientes" excat element={<Clientes />}/>
+                        </Routes>
+                    </Router>
+                </div>
+            </div>
+        </>
+    );
 };
 
-export default Routes;
+export default App;
