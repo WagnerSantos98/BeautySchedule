c326f2fa07b80697cec3a4b838ccb338ac3ccd6f
WagnerSantos98 <wagner.jesus98@outlook.com>
2023-12-20 20:18:36 -0300

    style: personalização do painel Web

--- web/src/components/Header/index.js
+++ web/src/components/Header/index.js
@@ -1,5 +1,16 @@
 const Header = () => {
-    return <h1>Header</h1>
+    return(
+        <header className="container-fluid d-flex justify-content-end">
+            <div className="d-flex align-items-center">
+                <div>
+                  <span className="d-block m-0 p-0 text-white">Beauty Schedule</span>
+                  <small className="m-0 p-0">Plano Gold</small>  
+                </div>
+                <img src="https://materializecss.com/images/yuna.jpg"/>
+                <span className="mdi mdi-chevron-down text-white"></span>
+            </div>
+        </header>
+    );
 };
 
 export default Header;
