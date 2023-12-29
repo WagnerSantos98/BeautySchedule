e8b65532bbb07769812351e2de16636fcb5d07d6
WagnerSantos98 <wagner.jesus98@outlook.com>
2023-12-29 18:42:41 -0300

    style: personalizando o sidebar

--- web/src/components/Sidebar/index.js
+++ web/src/components/Sidebar/index.js
@@ -1,19 +1,22 @@
 import { Link } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import logo from '../../assets/logo.png';
 
 const Sidebar = () => {
+    const location = useLocation();
+    
     return (
         <sidebar className="col-2 h-100">
             <img src={logo} className="img-fluid px-3 py-4"/>
-            <ul>
+            <ul className="p-0 m-0">
                 <li>
-                    <Link to="/">
+                    <Link to="/" className={location.pathname === '/' ? 'active' : ''}>
                         <span className="mdi mdi-calendar-check"></span>
                         <text>Agendamentos</text>
                     </Link>
                 </li>
                 <li>
-                    <Link to="/clientes">
+                    <Link to="/clientes" className={location.pathname === '/clientes' ? 'active' : ''}>
                         <span className="mdi mdi-account-multiple"></span>
                         <text>Clientes</text>
                     </Link>
