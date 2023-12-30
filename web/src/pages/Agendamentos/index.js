5e2b509d9c5b33ea0953e9d063d875eb5609aa31
WagnerSantos98 <wagner.jesus98@outlook.com>
2023-12-30 13:55:19 -0300

    feat: implementação de calendario e filtro para agendamentos

--- web/src/pages/Agendamentos/index.js
+++ web/src/pages/Agendamentos/index.js
@@ -1,11 +1,24 @@
+import { useEffect } from 'react';
 import { Calendar, momentLocalizer } from 'react-big-calendar';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 import moment from 'moment';
+import { useDispatch } from 'react-redux';
 
 
 const localizer = momentLocalizer(moment);
 
 const Agendamentos = () => {
+
+    const dispatch = useDispatch();
+
+    useEffect(() =>{
+        dispatch({
+            type: '@agendamento/FILTER',
+            start: moment().weekday(0).format('YYYY-MM-DD'),
+            end: moment().weekday(6).format('YYYY-MM-DD')
+        })
+    }, []);
+
     return (
         <div className="col p-5 overflow-auto h-100">
             <div className="row">
--- web/src/store/modules/agendamento/sagas.js
+++ web/src/store/modules/agendamento/sagas.js
@@ -1,5 +1,21 @@
-import { all } from 'redux-saga/effects';
+import { all, takeLatest, call } from 'redux-saga/effects';
+import api from '../../../services/api';
+import consts from '../../../../src/consts';
 
-export default all([
-    //@agendamento/ALL getAgendamentos
-]);
+export function* filterAgendamento({start, end}){
+    try{
+        const res = yield call(api.post, '/agendamento/filter', {
+            salaoId: consts.salaoId,
+            periodo:{
+                inicio: start,
+                final: end
+            },
+        });
+
+        console.log(res.data);
+    }catch(err){
+        alert(err.message);
+    }
+} 
+
+export default all([takeLatest('@agendamento/FILTER', filterAgendamento)]);
