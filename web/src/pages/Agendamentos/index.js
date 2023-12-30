78d24af9331069303ae3fa7d5c5a4023868c870a
WagnerSantos98 <wagner.jesus98@outlook.com>
2023-12-30 13:29:23 -0300

    feat: implementação react big calendar

--- web/src/pages/Agendamentos/index.js
+++ web/src/pages/Agendamentos/index.js
@@ -1,9 +1,30 @@
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import moment from 'moment';
+
+
+const localizer = momentLocalizer(moment);
+
 const Agendamentos = () => {
     return (
         <div className="col p-5 overflow-auto h-100">
             <div className="row">
                 <div className="col-12">
                     <h2 className="mb-4 mt-0">Agendamentos</h2>
+                    <Calendar
+                        localizer={localizer}
+                        events={[
+                            {
+                            title: "Evento Teste", 
+                            start: moment().toDate(), 
+                            end: moment().add(90, 'minutes').toDate(),
+                            }
+                        ]}
+                        defaultView="week"
+                        selectable
+                        popup
+                        style={{ height: 600 }}
+                    />
                 </div>
             </div>
         </div>
