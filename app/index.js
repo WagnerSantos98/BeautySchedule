56abb0c83bdea7344db6bc920eac9737c87528e3
WagnerSantos98 <wagner.jesus98@outlook.com>
2024-01-02 15:10:17 -0300

    feat: personalização do app

--- app/index.js
+++ app/index.js
@@ -3,14 +3,25 @@ import {AppRegistry} from 'react-native';
 import Home from './src/pages/Home';
 import {name as appName} from './app.json';
 
-import { Provider } from 'react-redux';
+import { Provider as StorProvider} from 'react-redux';
+import { DefaultTheme, configureFonts, Proveider as PaperProveider} from 'react-native-paper'; //Estilização do app
 import store from './src/store';
+
+const theme = {
+    ...DefaultTheme,
+    fonts: configureFonts({
+        ios: ,
+        android: ,
+    })
+}
 
 const App = () => {
     return (
-        <Provider store={store}>
-            <Home/>
-        </Provider>
+        <StorProvider store={store}>
+            <PaperProveider>
+                <Home/>
+            </PaperProveider>
+        </StorProvider>
     )
 }
 
