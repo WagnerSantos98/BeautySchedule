e7563a9c93ffd930ee13d057d9fbf160cc7c47f4
WagnerSantos98 <wagner.jesus98@outlook.com>
2024-01-05 16:27:11 -0300

    feat: implementação modal

--- app/src/components/ModalAgendamento/index.js
+++ app/src/components/ModalAgendamento/index.js
@@ -1,22 +1,19 @@
 import React from 'react';
-import BottomSheet from '@gorhom/bottom-sheet';
+import BottomSheet from 'reanimated-bottom-sheet';
 import { ScrollView, Dimensions } from 'react-native';
 import ModalHeader from './header';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 const ModalAgendamento = () => {
     return (
-        <GestureHandlerRootView>
             <BottomSheet
             initialSnap={1}
-            snapPoints={[30, 70, Dimensions.get('window').height - 30]}
+            snapPoints={[0, 70, Dimensions.get('screen').height - 30]}
             renderContent={() => (
                 <ScrollView style={{backgroundColor: '#fff'}}>
                     <ModalHeader/>
                 </ScrollView>
             )}
             />
-        </GestureHandlerRootView>
     );
 };
 
