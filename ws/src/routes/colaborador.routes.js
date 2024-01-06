3a586c5922a9b429453d9e0071d417085f373377
WagnerSantos98 <wagner.jesus98@outlook.com>
2024-01-06 18:33:42 -0300

    refactor: router colaborador, verificação de cadastro existente

--- ws/src/routes/colaborador.routes.js
+++ ws/src/routes/colaborador.routes.js
@@ -1,13 +1,84 @@
 const express = require('express');
 const router = express.Router();
 const Colaborador = require('../models/colaborador');
+const SalaoColaborador = require('../models/relationship/salaoColaborador');
+const ColaboradorServico = require('../models/relationship/colaboradorServico');
 
 router.post('/', async (req, res) => {
     try{
-        const colaborador = await new Colaborador(req.body).save();
-        res.json({ colaborador });
+
+        const { colaborador, salaoId } = req.body;
+        let newColaborador = null;
+
+        //Verificar se o colaborador existe
+        const existentColaborador = await Colaborador.findOne({
+            $or: [
+                { email: colaborador.email },
+                { telefone: colaborador.telefone },
+            ]
+            
+        });
+
+        //Se não existir o colaborador
+        if(!existentColaborador){
+            newColaborador = await Colaborador({
+                ...colaborador,
+            }).save();
+        }
+
+        //Relacionamento Colaborador/Salão
+        const colaboradorId = existentColaborador ? existentColaborador._id : newColaborador._id;
+
+        //Verificar se existe o relacionamento com salão
+        const existentRelatioship = await SalaoColaborador.findOne({
+            salaoId,
+            colaboradorId,
+            status: { $ne: 'E' },
+        });
+
+        //Se não está vinvulado
+        if(!existentRelatioship){
+            await new SalaoColaborador({
+                salaoId,
+                colaboradorId,
+                status: colaborador.vinculo,
+            }).save();
+        }
+
+        //Se já existir um vinculo colaborador/salão
+        if(existentColaborador){
+            const existentRelatioship = await SalaoColaborador.findOneAndUpdate({
+                salaoId,
+                colaboradorId,
+            }, 
+            { status: colaborador.vinculo }
+            );
+        }
+        
+        //Relação com as especialidades
+        await ColaboradorServico.insertMany(
+            colaborador.especialidades.map(servicoId => ({
+                servicoId,
+                colaboradorId,
+            }))
+        )
+
+        if(existentColaborador && existentRelatioship){
+            res.json({ error: true, message: 'Colaborador já cadastrado'});
+        }else{
+            res.json({ error: false });
+        }
+ 
     }catch(err){
-        res.json({error: true, message: err.message});
+        res.json({ error: true, message: err.message });
+    }
+});
+
+router.put('/:colaboradorId', async (req, res) => {
+    try{
+        
+    }catch(err){
+        res.json({ error: true, message: err.message })
     }
 });
 
