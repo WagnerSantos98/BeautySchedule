53fbef5abb544ee2377194df32433f9adef817f3
WagnerSantos98 <wagner.jesus98@outlook.com>
2024-01-07 13:43:43 -0300

    refactor: alteração de rota do cliente

--- ws/src/routes/cliente.routes.js
+++ ws/src/routes/cliente.routes.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const Cliente = require('../models/cliente');
-const Salao = require('../models/salao');
 const SalaoCliente = require('../models/relationship/salaoCliente');
 
 //Método de inserção de cliente
