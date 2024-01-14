4a5700c9a22886a93eef451bee91355f52649943
WagnerSantos98 <wagner.jesus98@outlook.com>
2024-01-14 17:28:50 -0300

    style: personalização de fontes e tema

--- app/index.js
+++ app/index.js
@@ -1,16 +1,29 @@
-
+import React from 'react';
 import {AppRegistry} from 'react-native';
 import Home from './src/pages/Home';
 import {name as appName} from './app.json';
-
-import { Provider } from 'react-redux';
+import { Provider as StoreProvider} from 'react-redux';
+import { DefaultTheme, configureFonts, Provider as PaperProvider} from 'react-native-paper';
 import store from './src/store';
+
+//Css
+import { fonts } from './src/styles/theme.json';
+
+const theme = {
+    ...DefaultTheme,
+    fonts: configureFonts({
+        ios: fonts,
+        android: fonts,
+    })
+};
 
 const App = () => {
     return (
-        <Provider store={store}>
-            <Home/>
-        </Provider>
+        <StoreProvider store={store}>
+            <PaperProvider theme={theme}>
+                <Home/>
+            </PaperProvider>
+        </StoreProvider>
     );
 };
 
