0a41441332eb51af335a130a4f6875d66b5846c8
WagnerSantos98 <wagner.jesus98@outlook.com>
2024-01-19 15:38:01 -0300

    refactor: atualização de routes salão

--- ws/src/routes/salao.routes.js
+++ ws/src/routes/salao.routes.js
@@ -40,7 +40,7 @@ router.get('/:id' ,async (req, res) =>{
         //Distância
         const distance = turf.distance(
             turf.point(salao.geo.coordinates),
-            turf.point([-22.9582103, -46.5478778])
+            turf.point([-22.955548, -46.5760525])
         );
         
         //Aberto
